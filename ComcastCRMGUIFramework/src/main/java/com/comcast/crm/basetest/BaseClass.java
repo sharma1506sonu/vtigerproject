fa33d25259992e88105a60f08a061ffa3898155e
sharma1506sonu <139525851+sharma1506sonu@users.noreply.github.com>
2025-10-14 13:33:08 +0530

    this is updated

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/basetest/BaseClass.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/basetest/BaseClass.java
@@ -14,7 +14,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
 
 import com.comcast.crm.generic.databaseutility.DataBaseUtility;
 import com.comcast.crm.generic.fileutility.FileUtility;
